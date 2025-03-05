a37ca13a5170994770f7ec162de0cc6b81d668b9
Rafa <rafacv23@gmail.com>
2025-03-05 12:53:51 +0100

    feat: add loading state management and extract into new components

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,17 @@
 import { useState } from "react"
-import reactLogo from "./assets/react.svg"
-import { invoke } from "@tauri-apps/api/core"
 import "./App.css"
 import { fetch } from "@tauri-apps/plugin-http"
+import Form from "./components/Form"
+import Hero from "./components/Hero"
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("")
-  const [name, setName] = useState("")
-  const [searchResults, setSearchResults] = useState(null)
+  const [query, setQuery] = useState<string>("")
+  const [loading, setLoading] = useState<boolean>(false)
+  const [searchResults, setSearchResults] = useState<string | null>(null)
 
-  async function greet() {
-    // Greet message
-    setGreetMsg(await invoke("greet", { name }))
-
-    const query = "resultado madrid atleti"
+  async function fetchData() {
     try {
+      setLoading(true)
       // Fetching data from the API
       const res = await fetch(
         `https://digger-browser.vercel.app/api/search?q=${query}`,
@@ -32,6 +29,8 @@ function App() {
       // Log response
       console.log(jsonData)
 
+      setLoading(false)
+
       // Update search results state
       setSearchResults(jsonData)
     } catch (error) {
@@ -41,45 +40,13 @@ function App() {
 
   return (
     <main className="container">
-      <h1>Welcome to Tauri + React</h1>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault()
-          greet()
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-      <p>{greetMsg}</p>
-
-      <div>
-        <h2>Search Results</h2>
-        {searchResults ? (
-          <pre>{JSON.stringify(searchResults, null, 2)}</pre>
-        ) : (
-          <p>No results yet</p>
-        )}
-      </div>
+      <Hero />
+      <Form setQuery={setQuery} fetchData={fetchData} />
+      {loading ? (
+        "Loading..."
+      ) : (
+        <pre>{JSON.stringify(searchResults, null, 2)}</pre>
+      )}
     </main>
   )
 }
