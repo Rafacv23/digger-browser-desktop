0ac15792bc9a7f68f58477895f9d5cff807dd154
Rafa <rafacv23@gmail.com>
2025-03-05 13:14:42 +0100

    feat: integrate lucide-react Search icon into Form component

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,3 +1,5 @@
+import {Search} from "lucide-react"
+
 interface FormProps {
   fetchData: () => void
   setQuery: (query: string) => void
@@ -17,7 +19,9 @@ export default function Form({ fetchData, setQuery }: FormProps) {
         onChange={(e) => setQuery(e.currentTarget.value)}
         placeholder="I will summarize this for you..."
       />
-      <button type="submit">Search</button>
+      <button type="submit">
+        <Search size={16}/>
+        Search</button>
     </form>
   )
 }
