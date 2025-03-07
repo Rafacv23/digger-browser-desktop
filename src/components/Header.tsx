4941525e935a68af9b24eee23e5a4ba1aa7b88ff
Rafa <rafacv23@gmail.com>
2025-03-07 11:42:31 +0100

    feat: add Header component with navigation links and update styling for improved layout

---
+++ src/components/Header.tsx
@@ -0,0 +1,30 @@
+import { WEBSITE } from "../lib/constants"
+
+export default function Header() {
+  return (
+    <header>
+      <div>
+        <a href={WEBSITE} target="_blank" title="Go to web version">
+          <img src="favicon.png" alt="Digger browser logo" width={60} />
+          Digger Browser
+        </a>
+      </div>
+      <div>
+        <a
+          href={`${WEBSITE}/about`}
+          target="_blank"
+          title="About in web version"
+        >
+          About
+        </a>
+        <a
+          href={`${WEBSITE}/donate`}
+          target="_blank"
+          title="Donate in web version"
+        >
+          Donate
+        </a>
+      </div>
+    </header>
+  )
+}
---
+++ src/lib/constants.ts
@@ -0,0 +1,3 @@
+export const GITHUB_REPO = "https://github.com/rafacv23/digger-browser"
+export const PORTFOLIO = "https://www.rafacanosa.dev"
+export const WEBSITE = "https://digger-browser.vercel.app"
