1347c52441b94f75294712bb36698a4c22bdcc13
Rafa <rafacv23@gmail.com>
2025-03-07 11:46:00 +0100

    feat: integrate Header and Footer components into main application render

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App"
+import Footer from "./components/Footer"
+import Header from "./components/Header"
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
+    <Header />
     <App />
-  </React.StrictMode>,
-);
+    <Footer />
+  </React.StrictMode>
+)
