3017b17a9bf65849f82aa04835469aed6d6ca135
Rafa <rafacv23@gmail.com>
2025-03-07 12:14:32 +0100

    feat: update Hero component to use constant for website link and enhance title styling

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,10 @@
+import { WEBSITE } from "../lib/constants"
+
 export default function Hero() {
   return (
-    <div>
+    <div className="hero">
       <div className="row">
-        <a href="https://digger-browser.vercel.app" target="_blank">
+        <a href={WEBSITE} target="_blank">
           <img
             src="/favicon.png"
             className="logo vite"
@@ -10,7 +12,10 @@ export default function Hero() {
           />
         </a>
       </div>
-      <h1>Welcome to Digger</h1>
+      <h1 className="title">
+        Welcome to
+        <span>Digger</span>
+      </h1>
       <p>The next generation browser that summarizes the web for you.</p>
     </div>
   )
